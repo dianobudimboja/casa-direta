6e0ee4b0b72c0642a9d32c8ff7a869a958b3f8aa
Diano C. Budimbo Já <diano.budimboja@gmail.com>
2026-05-17 02:16:26 +0100

    Fix: simplified password reset - delete cor

--- cor.py
+++ cor.py
@@ -1,14 +1,8 @@
 from app import create_app, db
-from app.models.user import User
 
 app = create_app()
-ctx = app.app_context()
-ctx.push()
 
-user = User.query.filter_by(email='diano.budimboja@gmail.com').first()
-user.is_admin = True
-db.session.commit()
-
-print(f'{user.email} agora é administrador!')
-
-ctx.pop()
+with app.app_context():
+    print(db.engine.url)
+    db.create_all()
+    print('✅ Conectado e tabelas criadas!')
